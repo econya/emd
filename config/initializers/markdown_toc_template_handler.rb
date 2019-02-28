adbffef310eb2536341031c3aae47fee61a06a55
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2019-02-28 23:05:55 +0100

    add a renderer leveraging redcarpets TOC renderer

---
+++ config/initializers/markdown_toc_template_handler.rb
@@ -0,0 +1,22 @@
+module MarkdownTocTemplateHandler
+  def self.erb
+    @erb ||= ActionView::Template.registered_template_handler(:erb)
+  end
+
+  def self.call(template)
+    compiled_source = erb.call(template)
+    "Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC,
+                             no_intra_emphasis: true,
+                             fenced_code_blocks: true,
+                             space_after_headers: true,
+                             smartypants:                  true,
+                             disable_indented_code_blocks: true,
+                             prettify:                     true,
+                             tables:                       true,
+                             with_toc_data:                true,
+                             autolink: true).render(begin;#{compiled_source};end).html_safe"
+  end
+end
+
+ActionView::Template.register_template_handler :md_toc, MarkdownTocTemplateHandler
+ActionView::Template.register_template_handler :markdown_toc, MarkdownTocTemplateHandler
